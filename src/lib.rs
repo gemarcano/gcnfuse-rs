9a601da73c3826648ab384f0baa88dd586e5fc4e
Gabriel Marcano <gabemarcano@yahoo.com>
2026-02-10 14:59:43 -0800

    Fix another read bug, refactor
     - Was not accounting for offsets when determining how much was left to
       read of a file, fixed
     - Split implementation into two main files now.
     - Changed user:group to root:root

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later OR GPL-2.0-or-later OR MPL-2.0
+// SPDX-FileCopyrightText: 2026 Gabriel Marcano <gabemarcano@yahoo.com>
+
+mod fuse;
+
+pub use fuse::GcnFuse;
